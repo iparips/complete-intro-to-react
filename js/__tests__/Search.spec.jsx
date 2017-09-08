f671011cee194849f2a78e79f2cb686997c62c22
Ilya Paripsa <ilya.paripsa@rea-group.com>
2017-09-08 11:36:33 +1000

    Add test filtering test

--- js/__tests__/Search.spec.jsx
+++ js/__tests__/Search.spec.jsx
@@ -5,13 +5,24 @@ import ShowCard from "../ShowCard";
 import preload from "../../data.json";
 
 describe("Search", () => {
-  test("Search renders", () => {
+  test("search renders", () => {
     const component = shallow(<Search />);
     expect(component).toMatchSnapshot();
   });
 
   test("search should render correct number of shows", () => {
     const component = shallow(<Search />);
-    expect(component.find(ShowCard).length).toEqual(preload.shows.length);
+    expect(component.find(ShowCard).length).toEqual(15);
+  });
+
+  test("should render two shows given term 'black'", () => {
+    const component = shallow(<Search />);
+    const searchTerm = "black";
+
+    component
+      .find("input")
+      .simulate("change", { target: { value: searchTerm } });
+
+    expect(component.find(ShowCard).length).toEqual(2);
   });
 });
