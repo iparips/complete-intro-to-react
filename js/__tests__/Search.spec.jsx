3cb144f23e534a0799f992bc254477196103f13c
Ilya Paripsa <ilya.paripsa@rea-group.com>
2017-09-08 11:26:29 +1000

    Add enzyme for shallow testing

--- js/__tests__/Search.spec.jsx
+++ js/__tests__/Search.spec.jsx
@@ -1,9 +1,17 @@
 import React from "react";
-import renderer from "react-test-renderer";
+import { shallow } from "enzyme";
 import Search from "../Search";
+import ShowCard from "../ShowCard";
+import preload from "../../data.json";
 
-test("Search renders", () => {
-  const component = renderer.create(<Search />);
-  const tree = component.toJSON();
-  expect(tree).toMatchSnapshot();
+describe("Search", () => {
+  test("Search renders", () => {
+    const component = shallow(<Search />);
+    expect(component).toMatchSnapshot();
+  });
+
+  test("search should render correct number of shows", () => {
+    const component = shallow(<Search />);
+    expect(component.find(ShowCard).length).toEqual(preload.shows.length);
+  });
 });
