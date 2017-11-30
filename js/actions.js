52871efe552cbbc3bf19e67d8ae5e400540f7c4c
Ilya Paripsa <ilya.paripsa@rea-group.com>
2017-11-30 15:01:03 +1100

    Add flow to redux files

--- js/actions.js
+++ js/actions.js
@@ -1,3 +1,5 @@
+// @flow
+
 /* jshint ignore:start*/
 export const SET_SEARCH_TERM = 'SET_SEARCH_TERM';
 /* jshint ignore:end */
--- js/reducers.js
+++ js/reducers.js
@@ -1,21 +1,17 @@
+// @flow
+
+import { combineReducers } from 'redux';
 import { SET_SEARCH_TERM } from "./actions";
 
-const DEFAULT_STATE = {
-  searchTerm: 'bob'
-};
+const searchTerm = (state='', action: Action) => {
 
-const setSearchTerm = (state, action) =>
-  Object.assign({}, state, { searchTerm: action.payload });
-
-const rootReducer = (state = DEFAULT_STATE, action) => {
-
-  switch (action.type) {
-    case SET_SEARCH_TERM:
-      return setSearchTerm(state, action);
-    default:
-      return state;
+  if (action.type === SET_SEARCH_TERM) {
+    return action.payload;
   }
 
+  return state;
 };
+
+const rootReducer = combineReducers({ searchTerm });
 
 export default rootReducer;
